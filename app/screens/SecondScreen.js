e3ed0e729601b4028c4f5d7155d96d8887215c85
Gaurav Roy <gauravroy@Gauravs-MacBook-Pro.local>
2020-10-04 12:35:58 +0530

    feat(timeline): added timeline props

--- app/screens/SecondScreen.js
+++ app/screens/SecondScreen.js
@@ -4,79 +4,85 @@ import {Header, BottomTab, SecondHeader} from '../components/globalComponents';
 import {firstScreenStyles} from '../style/firstScreenStyle';
 import {secondScreenStyles} from '../style/secondScreenStyles';
 import Timeline from 'react-native-timeline-flatlist';
+import {colors} from '../style/colors';
 
 const dataForTimeline = [
   {
     time: '09:00',
     title: 'Archery Training',
     description: 'Completed',
-    lineColor: '#009688',
+    lineColor: colors.lineColor,
     icon: require('../assets/images/check.png'),
   },
   {
     time: '10:45',
     title: 'Play Badminton',
     description: 'Completed',
+    lineColor: colors.lineColor,
     icon: require('../assets/images/check.png'),
   },
   {
     time: '12:00',
     title: 'Lunch',
+    description: '1 Min 20 Se',
+    lineColor: colors.lineBold,
     icon: require('../assets/images/current.png'),
   },
   {
     time: '14:00',
     title: 'Watch Soccer',
     description: 'Completed',
-    lineColor: '#009688',
+    lineColor: colors.lineBold,
     icon: require('../assets/images/locked.png'),
   },
   {
     time: '16:30',
     title: 'Go to Fitness center',
     description: 'Completed',
+    lineColor: colors.lineBold,
     icon: require('../assets/images/locked.png'),
   },
 ];
+
+const renderTimeLine = () => {
+  return (
+    <Timeline
+      showTime={false}
+      innerCircle={'icon'}
+      lineWidth={1}
+      data={dataForTimeline}
+      titleStyle={[
+        {
+          paddingTop: 0,
+          marginLeft: 20,
+          marginTop: Platform.OS === 'android' ? -12 : -10,
+        },
+      ]}
+      listViewStyle={{marginLeft: 10, marginTop: 20}}
+      timeStyle={{paddingLeft: 10, borderRadius: 13, padding: 0}}
+      descriptionStyle={{
+        color: 'gray',
+        paddingTop: 0,
+        marginBottom: 20,
+        marginLeft: 20,
+        fontSize: 12,
+        margin: 0,
+      }}
+      iconStyle={{
+        backgroundColor: '#fff',
+        width: 18,
+        height: 18,
+        marginTop: 2,
+      }}
+    />
+  );
+};
 
 const Card = () => {
   return (
     <View style={secondScreenStyles.cardW}>
       {CardHeader()}
-
-      <Timeline
-        showTime={false}
-        innerCircle={'icon'}
-        lineWidth={1}
-        // onEventPress={async obj => { this.checkTimeoutFunc(obj, topicDetails); }}
-        data={dataForTimeline}
-        // onPress={async obj => { this.checkTimeoutFunc(obj, topicDetails); }}
-        titleStyle={[
-          {
-            paddingTop: 0,
-            marginLeft: 20,
-            marginTop: Platform.OS === 'android' ? -12 : -10,
-          },
-        ]}
-        // listViewStyle={{ fontFamily: fontType.gilroyRegular }}
-        // detailContainerStyle={{ fontFamily: fontType.gilroyRegular }}
-        timeStyle={{paddingLeft: 10, borderRadius: 13, padding: 0}}
-        descriptionStyle={{
-          color: 'gray',
-          paddingTop: 0,
-          marginBottom: 20,
-          marginLeft: 20,
-          fontSize: 12,
-          margin: 0,
-        }}
-        iconStyle={{
-          backgroundColor: '#fff',
-          width: 18,
-          height: 18,
-          marginTop: 2,
-        }}
-        separatorStyle={{borderStyle: 'dashed'}}
-      />
+      {renderTimeLine()}
     </View>
   );
 };
--- app/style/colors.js
+++ app/style/colors.js
@@ -1,4 +1,6 @@
 export const colors = {
   theme: '#3a44f9',
   white: '#fff',
+  lineColor: '#50e3c2',
+  lineBold: '#ccc',
 };
